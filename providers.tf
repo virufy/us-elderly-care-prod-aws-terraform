904fae712cdcd70973cad9181e32aea4e2b55353
moonnada <kimbm2014@gmail.com>
2025-11-18 11:54:59 -0800

    Add AWS provider ver in providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  
+    }
+  }
+}
+
+# AWS Provider
 variable "aws_profile" {
   type    = string
   default = "mothership-prod"
