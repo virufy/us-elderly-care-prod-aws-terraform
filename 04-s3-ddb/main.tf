7270eafedee4bc22ca9ed45ce10282d11fe11509
moonnada <kimbm2014@gmail.com>
2025-11-18 16:33:51 -0800

    Make SSE for S3

--- 04-s3-ddb/main.tf
+++ 04-s3-ddb/main.tf
@@ -1,28 +1,41 @@
 # S3 for raw audio files
 resource "aws_s3_bucket" "raw_audio" {
-    bucket = "${var.project}-${var.env}-raw-audio"
+  bucket = "${var.project}-${var.env}-raw-audio"
 }
 
+# Enable SSE-KMS
+resource "aws_s3_bucket_server_side_encryption_configuration" "raw_audio" {
+  bucket = aws_s3_bucket.raw_audio.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_bucket_key.arn
+    }
+  }
+}
+
+# Block public access
 resource "aws_s3_bucket_public_access_block" "raw_audio" {
-    bucket = aws_s3_bucket.raw_audio.id
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.raw_audio.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # DynamoDB for metadata
 resource "aws_dynamodb_table" "metadata" {
-    name = "${var.project}-${var.env}-metadata"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "PatientID"
+  name         = "${var.project}-${var.env}-metadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PatientID"
 
-    attribute {
-        name = "PatientID"
-        type = "S"
-    }
+  attribute {
+    name = "PatientID"
+    type = "S"
+  }
 
-    server_side_encryption {
-        enabled = true
-    }
+  server_side_encryption {
+    enabled = true
+  }
 }
